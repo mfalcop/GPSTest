94596cd408cb878028a25f1fe70767337e4ac4e0
mfalcop <melanyfalco@gmail.com>
2017-05-04 22:41:38 -0500

    Initial commit

---
+++ app/src/main/java/com/example/melanis/gpstest/MainActivity.java
@@ -0,0 +1,101 @@
+package com.example.melanis.gpstest;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiActivity;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+
+public class MainActivity extends AppCompatActivity implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener, LocationListener {
+
+    private String TAG = "SampleGoogleAPI";
+    private GoogleApiClient mGoogleApiClient;
+    private LocationRequest mLocationRequest;
+    private static final long UPDATE_INTERVAL_IN_MLS = 1000;
+    private static final long FASTEST_UPDATE_INTERVAL_IN_MLS = UPDATE_INTERVAL_IN_MLS / 2;
+
+
+    TextView tvLat,tvLong;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        tvLat= (TextView) findViewById(R.id.textView2);
+        tvLong= (TextView) findViewById(R.id.textView3);
+
+
+        mGoogleApiClient = new GoogleApiClient.Builder(this).addConnectionCallbacks(this).addOnConnectionFailedListener(this).addApi(LocationServices.API).build();
+
+        mLocationRequest = new LocationRequest();
+        mLocationRequest.setInterval(UPDATE_INTERVAL_IN_MLS);
+        mLocationRequest.setFastestInterval(FASTEST_UPDATE_INTERVAL_IN_MLS);
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_BALANCED_POWER_ACCURACY);
+
+
+    }
+
+
+    public void onClickGO(View view) {
+        if (mGoogleApiClient.isConnected()) {
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(this,"???",Toast.LENGTH_LONG).show();
+                return;
+            }
+            LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, mLocationRequest, this);
+        }
+
+
+    }
+
+    public void onClickSTOP(View view) {
+        LocationServices.FusedLocationApi.removeLocationUpdates(mGoogleApiClient,this);
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        if(mGoogleApiClient!= null){
+            mGoogleApiClient.connect();
+
+        }
+    }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        Log.d(TAG,"OnConnected");
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+        Log.d(TAG,"onConnectionSuspended");
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+        Toast.makeText(this,"onConnectionFailed",Toast.LENGTH_LONG).show();
+
+        Log.d(TAG,"onConnectionFailed"+connectionResult.toString());
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        tvLat.setText(location.getLatitude()+"");
+        tvLong.setText(location.getLongitude()+"");
+    }
+}
