3b6946f32167ded00be423760c5fd6735f424239
mfalcop <melanyfalco@gmail.com>
2017-05-07 18:33:39 -0500

    Permissions added

--- app/src/main/java/com/example/melanis/gpstest/MainActivity.java
+++ app/src/main/java/com/example/melanis/gpstest/MainActivity.java
@@ -16,26 +16,32 @@ import android.widget.Toast;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiActivity;
 import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.PendingResult;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.common.api.Status;
 import com.google.android.gms.location.LocationListener;
 import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.LocationSettingsRequest;
+import com.google.android.gms.location.LocationSettingsResult;
+import com.google.android.gms.location.LocationSettingsStatusCodes;
 
-public class MainActivity extends AppCompatActivity implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener, LocationListener {
+public class MainActivity extends AppCompatActivity implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener, LocationListener, ResultCallback<LocationSettingsResult> {
 
     private String TAG = "SampleGoogleAPI";
     private GoogleApiClient mGoogleApiClient;
     private LocationRequest mLocationRequest;
     private static final long UPDATE_INTERVAL_IN_MLS = 1000;
     private static final long FASTEST_UPDATE_INTERVAL_IN_MLS = UPDATE_INTERVAL_IN_MLS / 2;
+    private  LocationSettingsRequest mLocationSettingsRequest;
+    TextView tvLat, tvLong;
 
-
-    TextView tvLat,tvLong;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        tvLat= (TextView) findViewById(R.id.textView2);
-        tvLong= (TextView) findViewById(R.id.textView3);
+        tvLat = (TextView) findViewById(R.id.textView2);
+        tvLong = (TextView) findViewById(R.id.textView3);
 
 
         mGoogleApiClient = new GoogleApiClient.Builder(this).addConnectionCallbacks(this).addOnConnectionFailedListener(this).addApi(LocationServices.API).build();
@@ -50,9 +56,22 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
 
 
     public void onClickGO(View view) {
+        LocationSettingsRequest.Builder builder = new LocationSettingsRequest.Builder();
+        builder.addLocationRequest(mLocationRequest);
+        mLocationSettingsRequest = builder.build();
+
+        PendingResult<LocationSettingsResult> result =
+                LocationServices.SettingsApi.checkLocationSettings(
+                        mGoogleApiClient,
+                        mLocationSettingsRequest
+                );
+        result.setResultCallback(this);
+
+
+
         if (mGoogleApiClient.isConnected()) {
             if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-                Toast.makeText(this,"???",Toast.LENGTH_LONG).show();
+                Toast.makeText(this, "???", Toast.LENGTH_LONG).show();
                 return;
             }
             LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, mLocationRequest, this);
@@ -62,14 +81,14 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
     }
 
     public void onClickSTOP(View view) {
-        LocationServices.FusedLocationApi.removeLocationUpdates(mGoogleApiClient,this);
+        LocationServices.FusedLocationApi.removeLocationUpdates(mGoogleApiClient, this);
 
     }
 
     @Override
     protected void onStart() {
         super.onStart();
-        if(mGoogleApiClient!= null){
+        if (mGoogleApiClient != null) {
             mGoogleApiClient.connect();
 
         }
@@ -77,7 +96,16 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
 
     @Override
     public void onConnected(@Nullable Bundle bundle) {
-        Log.d(TAG,"OnConnected");
+        Log.d(TAG, "OnConnected");
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            Toast.makeText(this, "???", Toast.LENGTH_LONG).show();
+            return;
+        }
+        Location lastposition = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+        if(lastposition != null){
+            tvLat.setText(lastposition.getLatitude()+"");
+            tvLong.setText(lastposition.getLongitude()+"");
+        }
     }
 
     @Override
@@ -97,5 +125,29 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
     public void onLocationChanged(Location location) {
         tvLat.setText(location.getLatitude()+"");
         tvLong.setText(location.getLongitude()+"");
+    }
+
+    @Override
+    public void onResult(@NonNull LocationSettingsResult locationSettingsResult) {
+        final Status status= locationSettingsResult.getStatus();
+        switch (status.getStatusCode()){
+            case LocationSettingsStatusCodes.SUCCESS:
+                if (mGoogleApiClient.isConnected()) {
+                    if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                        Toast.makeText(this, "???", Toast.LENGTH_LONG).show();
+                        return;
+                    }
+                    LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, mLocationRequest, this);
+                }
+                Location lastposition = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+                break;
+            case LocationSettingsStatusCodes.RESOLUTION_REQUIRED:
+                Toast.makeText(this,"LocationSettingsStatusCodes.RESOLUTION_REQUIRED",Toast.LENGTH_LONG).show();
+                break;
+            case LocationSettingsStatusCodes.SETTINGS_CHANGE_UNAVAILABLE:
+                Toast.makeText(this,"LocationSettingsStatusCodes.SETTINGS_CHANGE_UNAVAILABLE",Toast.LENGTH_LONG).show();
+
+                break;
+        }
     }
 }
